57ed6128051156c3462e580901ad9fd06a1ec2cc
Matheus Oliveira <matheusoliveira18rj@gmail.com>
2024-09-17 16:16:44 -0300

    feat: grid layout

---
+++ src/routes/Login.jsx
@@ -0,0 +1,16 @@
+import '../styles/login.css'
+
+function Login() {
+    return(
+        <main>
+            <section id='greetings-container'>
+                teste
+            </section>
+            <section id='login-container'>
+                teste
+            </section>
+        </main>
+    )
+}
+
+export default Login
