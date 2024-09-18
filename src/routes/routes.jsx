cc0229d1413b55c6e57b2f2aa922da173936cc1f
Matheus Oliveira <matheusoliveira18rj@gmail.com>
2024-09-18 16:31:38 -0300

    feat: register page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,8 +1,9 @@
 import Login from "../pages/Login"
+import Register from "../pages/Register"
 
 const routes = [
     {path: '/login', element: <Login />},
-    {path: '/cadastro', element: <>.</>}
+    {path: '/cadastro', element: <Register />}
 ]
 
 export default routes
