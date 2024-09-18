d9df48d089bd8ff1ff700e7283c3c08b660d3ae1
Matheus Oliveira <matheusoliveira18rj@gmail.com>
2024-09-18 17:35:28 -0300

    fix: nickname isn't mandatory

--- src/components/RegisterForm/schema.js
+++ src/components/RegisterForm/schema.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 const schema = Yup.object().shape({
     fullName: Yup.string().required('O nome completo é obrigatório'),
-    nickname: Yup.string().required('O nome completo é obrigatório'),
+    nickname: Yup.string(),
     email: Yup.string().required('O nome completo é obrigatório'),
     password: Yup.string().min(6, 'A senha deve ter pelo menos 6 caracteres').required('Senha é obrigatória'),
     confirmPassword: Yup.string().min(6, 'A senha deve ter pelo menos 6 caracteres').required('Senha é obrigatória')
